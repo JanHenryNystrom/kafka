53b533f0690bc36b804817f1409a1a24a893f1a3
Henry Nystrom <JanHenryNystrom@gmail.com>
2013-04-11 09:07:23 +0200

    More tests for fetch.

--- test/kafka_protocol_tests.erl
+++ test/kafka_protocol_tests.erl
@@ -127,6 +127,17 @@
           <<0,0,0,49,0,0,0,0,0,0,0,14,0,7,99,108,105,101,110,116,49,
             0,0,0,0,0,100,0,0,0,1,0,8,109,121,116,111,112,105,99,49,
             0,0,0,1,0,0,0,0,0,0,0,0>>},
+         {fetch, 92, "myId",
+          #fetch{timeout = 1000,
+                 min_bytes = 20,
+                 topics = [#topic{name = <<"mytopic1">>,
+                                  partitions=[#partition{
+                                                 id = 0,
+                                                 offset = 0,
+                                                 max_bytes = 1048576}]}]},
+         <<0,0,0,60,0,1,0,0,0,0,0,92,0,4,109,121,73,100,255,255,
+           255,255,0,0,3,232,0,0,0,20,0,0,0,1,0,8,109,121,116,111,
+           112,105,99,49,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,16,0,0>>},
          {offset, 15, "client1",
           #offset{replica = 0,
                   topics =
@@ -186,6 +197,177 @@
                              offset = [0]}]}]},
           <<0,0,0,40,0,0,0,0,0,0,0,1,0,8,109,121,116,111,112,105,99,49,0,
             0,0,1,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0>>},
+         {fetch,
+          #fetch_response{
+             corr_id = 92,
+             topics =
+                 [#topic_response{
+                     name = <<"mytopic1">>,
+                     partitions =
+                         [#partition_response{
+                             id = 0,error_code = 'NoError',leader = undefined,
+                             replicas = undefined,isrs = undefined,
+                             offset = undefined,high_watermark = 10,
+                             set =
+                                 [#set_response{
+                                     offset = 0,magic = undefined,
+                                     attributes = undefined,key = undefined,
+                                     value = undefined,
+                                     error_code = 'InvalidMessage'},
+                                  #set_response{
+                                     offset = 1,magic = undefined,
+                                     attributes = undefined,key = undefined,
+                                     value = undefined,
+                                     error_code = 'InvalidMessage'},
+                                  #set_response{
+                                     offset = 2,magic = undefined,
+                                     attributes = undefined,key = undefined,
+                                     value = undefined,
+                                     error_code = 'InvalidMessage'},
+                                  #set_response{
+                                     offset = 3,
+                                     magic = undefined,
+                                     attributes = undefined,
+                                     key = undefined,
+                                     value = undefined,
+                                     error_code = 'InvalidMessage'},
+                                  #set_response{
+                                     offset = 4,
+                                     magic = 1,
+                                     attributes = 0,
+                                     key = <<>>,
+                                     value = <<"foo">>,error_code = 'NoError'},
+                                  #set_response{
+                                     offset = 5,
+                                     magic = 1,
+                                     attributes = 0,
+                                     key = <<>>,
+                                     value = <<"foo">>,
+                                     error_code = 'NoError'},
+                                  #set_response{
+                                     offset = 6,
+                                     magic = 1,
+                                     attributes = 0,
+                                     key = <<>>,
+                                     value = <<"foo">>,
+                                     error_code = 'NoError'},
+                                  #set_response{
+                                     offset = 7,
+                                     magic = 1,
+                                     attributes = 0,
+                                     key = <<>>,
+                                     value = <<"foo">>,
+                                     error_code = 'NoError'},
+                                  #set_response{
+                                     offset = 8,
+                                     magic = 1,
+                                     attributes = 0,
+                                     key = <<>>,
+                                     value = <<"foo">>,
+                                     error_code = 'NoError'},
+                                  #set_response{
+                                     offset = 9,
+                                     magic = 1,
+                                     attributes = 0,
+                                     key = <<>>,
+                                     value = <<"foo">>,
+                                     error_code = 'NoError'}]}],
+                     error_code = 'NoError'}]},
+          <<0,0,1,74,0,0,0,92,0,0,0,1,
+            0,8,109,121,116,111,112,105,99,49,0,0,0,1,
+            0,0,0,0,0,0,0,0,0,0,0,0,0,10,0,0,1,34,
+            0,0,0,0,0,0,0,0,0,0,0,17,179,101,140,218,1,0,0,0,0,0,0,0,0,3,
+            102,111,111,0,0,0,0,0,0,0,1,0,0,0,17,179,101,140,218,1,0,0,0,
+            0,0,0,0,0,3,102,111,111,0,0,0,0,0,0,0,2,0,0,0,17,179,101,140,
+            218,1,0,0,0,0,0,0,0,0,3,102,111,111,0,0,0,0,0,0,0,3,0,0,0,17,
+            179,101,140,218,1,0,0,0,0,0,0,0,0,3,102,111,111,0,0,0,0,0,0,
+            0,4,0,0,0,17,39,29,194,201,1,0,255,255,255,255,0,0,0,3,102,
+            111,111,0,0,0,0,0,0,0,5,0,0,0,17,39,29,194,201,1,0,255,255,
+            255,255,0,0,0,3,102,111,111,0,0,0,0,0,0,0,6,0,0,0,17,39,29,
+            194,201,1,0,255,255,255,255,0,0,0,3,102,111,111,0,0,0,0,0,0,
+            0,7,0,0,0,17,39,29,194,201,1,0,255,255,255,255,0,0,0,3,102,
+            111,111,0,0,0,0,0,0,0,8,0,0,0,17,39,29,194,201,1,0,255,255,
+            255,255,0,0,0,3,102,111,111,0,0,0,0,0,0,0,9,0,0,0,17,39,29,
+            194,201,1,0,255,255,255,255,0,0,0,3,102,111,111>>},
+         {fetch,
+          #fetch_response{
+             corr_id = 92,
+             topics =
+                 [#topic_response{
+                     name = <<"mytopic1">>,
+                     partitions =
+                         [#partition_response{
+                             id = 0,error_code = 'NoError',leader = undefined,
+                             replicas = undefined,isrs = undefined,
+                             offset = undefined,high_watermark = 11,
+                             set =
+                                 [#set_response{
+                                     offset = 0,magic = undefined,
+                                     attributes = undefined,key = undefined,
+                                     value = undefined,
+                                     error_code = 'InvalidMessage'},
+                                  #set_response{
+                                     offset = 1,magic = undefined,
+                                     attributes = undefined,key = undefined,
+                                     value = undefined,
+                                     error_code = 'InvalidMessage'},
+                                  #set_response{
+                                     offset = 2,magic = undefined,
+                                     attributes = undefined,key = undefined,
+                                     value = undefined,
+                                     error_code = 'InvalidMessage'},
+                                  #set_response{
+                                     offset = 3,magic = undefined,
+                                     attributes = undefined,key = undefined,
+                                     value = undefined,
+                                     error_code = 'InvalidMessage'},
+                                  #set_response{
+                                     offset = 4,magic = 1,attributes = 0,
+                                     key = <<>>,
+                                     value = <<"foo">>,error_code = 'NoError'},
+                                  #set_response{
+                                     offset = 5,magic = 1,attributes = 0,
+                                     key = <<>>,
+                                     value = <<"foo">>,error_code = 'NoError'},
+                                  #set_response{
+                                     offset = 6,magic = 1,attributes = 0,
+                                     key = <<>>,
+                                     value = <<"foo">>,error_code = 'NoError'},
+                                  #set_response{
+                                     offset = 7,magic = 1,attributes = 0,
+                                     key = <<>>,
+                                     value = <<"foo">>,error_code = 'NoError'},
+                                  #set_response{
+                                     offset = 8,magic = 1,attributes = 0,
+                                     key = <<>>,
+                                     value = <<"foo">>,error_code = 'NoError'},
+                                  #set_response{
+                                     offset = 9,magic = 1,attributes = 0
+                                     ,key = <<>>,
+                                     value = <<"foo">>,error_code = 'NoError'},
+                                  #set_response{
+                                     offset = 10,magic = 1,attributes = 0,
+                                     key = <<"bar">>,value = <<"baz">>,
+                                     error_code = 'NoError'}]}],
+                     error_code = 'NoError'}]},
+          <<0,0,1,106,0,0,0,92,0,0,0,1,0,8,109,121,116,111,112,105,99,49,0,0,0,
+            1,
+            0,0,0,0,0,0,0,0,0,0,0,0,0,11,0,0,1,66,
+            0,0,0,0,0,0,0,0,0,0,0,17,179,101,140,218,1,0,0,0,0,0,0,0,0,3,
+            102,111,111,0,0,0,0,0,0,0,1,0,0,0,17,179,101,140,218,1,0,0,0,
+            0,0,0,0,0,3,102,111,111,0,0,0,0,0,0,0,2,0,0,0,17,179,101,140,
+            218,1,0,0,0,0,0,0,0,0,3,102,111,111,0,0,0,0,0,0,0,3,0,0,0,17,
+            179,101,140,218,1,0,0,0,0,0,0,0,0,3,102,111,111,0,0,0,0,0,0,
+            0,4,0,0,0,17,39,29,194,201,1,0,255,255,255,255,0,0,0,3,102,
+            111,111,0,0,0,0,0,0,0,5,0,0,0,17,39,29,194,201,1,0,255,255,
+            255,255,0,0,0,3,102,111,111,0,0,0,0,0,0,0,6,0,0,0,17,39,29,
+            194,201,1,0,255,255,255,255,0,0,0,3,102,111,111,0,0,0,0,0,0,
+            0,7,0,0,0,17,39,29,194,201,1,0,255,255,255,255,0,0,0,3,102,
+            111,111,0,0,0,0,0,0,0,8,0,0,0,17,39,29,194,201,1,0,255,255,
+            255,255,0,0,0,3,102,111,111,0,0,0,0,0,0,0,9,0,0,0,17,39,29,
+            194,201,1,0,255,255,255,255,0,0,0,3,102,111,111,0,0,0,0,0,0,
+            0,10,0,0,0,20,62,158,25,230,1,0,0,0,0,3,98,97,114,0,0,0,3,98,
+            97,122>>},
          {offset,
           #offset_response{
              corr_id = 0,
